8b91e520a8ef8b1d3cc8cede2d10f57946ef065d
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-03-04 00:08:31 +0530

    initial project setup

---
+++ server/src/main/java/com/nexmeet/nexmeet_service/NexMeetApplication.java
@@ -0,0 +1,13 @@
+package com.nexmeet.nexmeet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexMeetApplication.class, args);
+	}
+
+}
