72e9a213fa3650185b0e7d1d2a97a7b06e62c46e
Somnesh Mukhopadhyay <somneshmukhopadhyay@gmail.com>
2025-03-29 22:14:19 +0530

    added dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AuthPage from "./pages/AuthPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import VideoCallInterface from "./pages/VideoCallInterface";
+import { Toaster } from "./components/ui/sonner";
+import { ThemeProvider } from "./contexts/Theme";
 
 const router = createBrowserRouter([
   {
@@ -20,9 +22,39 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [theme, setTheme] = useState(
+    localStorage.theme === "white" || !("theme" in localStorage)
+      ? "white"
+      : "dark"
+  );
+  const [triggerElement, setTriggerElement] = useState(null);
+
+  const darkTheme = (e) => {
+    setTheme("dark");
+    localStorage.theme = "dark";
+    setTriggerElement(e);
+  };
+
+  const lightTheme = (e) => {
+    setTheme("white");
+    localStorage.theme = "white";
+    setTriggerElement(e);
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "white");
+    document.querySelector("html").classList.add(theme);
+    if (triggerElement) {
+      triggerElement.currentTarget.classList.add("dark:bg-white");
+    }
+  }, [theme]);
+
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider value={{ theme, darkTheme, lightTheme }}>
+        <Toaster />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
