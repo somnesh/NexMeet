2eca3aeaeba9b30a816c3f6c01f26e50917b73a4
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-05-03 15:53:19 +0530

    feat: update JoinMeetingResponse DTO to include meeting status, participant status, and participant ID

--- server/src/main/java/com/nexmeet/dto/JoinMeetingResponse.java
+++ server/src/main/java/com/nexmeet/dto/JoinMeetingResponse.java
@@ -1,12 +1,18 @@
 package com.nexmeet.dto;
 
 import com.nexmeet.model.MeetingStatus;
+import com.nexmeet.model.Participant;
+import com.nexmeet.model.ParticipantStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 public class JoinMeetingResponse {
     private String code;
-    private MeetingStatus currentStatus;
+    private MeetingStatus meetingStatus;
+    private ParticipantStatus participantStatus;
+    private UUID participantId;
 }
