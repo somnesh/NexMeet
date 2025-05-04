b9f104f21e602a2889faf4984653da7064e40812
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-05-04 12:49:19 +0530

    feat: add endMeeting method to ParticipantRepository for marking participants as left

--- server/src/main/java/com/nexmeet/repository/ParticipantRepository.java
+++ server/src/main/java/com/nexmeet/repository/ParticipantRepository.java
@@ -1,13 +1,21 @@
 package com.nexmeet.repository;
 
+import com.nexmeet.model.Meeting;
 import com.nexmeet.model.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
-    Optional<Participant> findByMeetingId(UUID meetingId);
     Optional<Participant> findById(UUID id);
-//    void deleteByMeetingId(String meetingId);
-}
+    void deleteByMeeting(Meeting meeting);
+
+    @Modifying
+    @Query("update Participant p set p.leftAt = CURRENT_TIMESTAMP where p.meeting.id = :meetingId")
+    void endMeeting(@Param("meetingId") UUID meetingId);
+    }
