c2e5dfc11a49e9a2013aa07ebc1f5c0654ba5eaa
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-05-12 00:14:47 +0530

    fix: add email field to AuthResponse constructor for complete user information

--- server/src/main/java/com/nexmeet/dto/AuthResponse.java
+++ server/src/main/java/com/nexmeet/dto/AuthResponse.java
@@ -8,14 +8,16 @@ import lombok.Setter;
 public class AuthResponse {
     private String name;
     private String id;
+    private String email;
     private String avatar;
     private String accessToken;
     private String refreshToken;
     private String message;
 
-    public AuthResponse(String name, String id, String avatar, String accessToken, String refreshToken, String message) {
+    public AuthResponse(String name, String id, String email,String avatar, String accessToken, String refreshToken, String message) {
         this.name = name;
         this.id = id;
+        this.email = email;
         this.avatar = avatar;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
