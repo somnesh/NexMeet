39c9135504b363745f22ae2ca6c29be7a522022b
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-05-24 11:10:21 +0530

    fix: update allowed origins in CORS and WebSocket configurations for consistency

--- server/src/main/java/com/nexmeet/config/WebSocketConfig.java
+++ server/src/main/java/com/nexmeet/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:63342","http://localhost:5173", "https://8vmq5xq3-5173.inc1.devtunnels.ms/") // Match your CORS config
+                .setAllowedOrigins("http://localhost:5173", "https://nex-meet-theta.vercel.app")
                 .withSockJS();
     }
 
