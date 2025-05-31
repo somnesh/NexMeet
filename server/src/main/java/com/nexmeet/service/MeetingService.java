94ac874f8053873de14c6ec0a0e4b007c3239a7e
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-05-31 22:49:58 +0530

    refactor: improve code formatting and readability in MeetingService

--- server/src/main/java/com/nexmeet/service/MeetingService.java
+++ server/src/main/java/com/nexmeet/service/MeetingService.java
@@ -52,17 +52,17 @@ public class MeetingService {
 
     public GetMeetingResponse getMeetingByCode(String code, String userEmail) {
         Meeting meeting = meetingRepository.findByCode(code)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Meeting not found"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Meeting not found"));
 
         boolean isHost = meeting.getHost().getEmail().equals(userEmail);
-        return new GetMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(), meeting.getMediaRoomId(), isHost);
+        return new GetMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(),
+                meeting.getMediaRoomId(), isHost);
     }
 
     @Transactional
     public CreateMeetingResponse createMeeting(CreateMeetingRequest request, String userEmail) {
         User host = userRepository.findByEmail(userEmail).orElseThrow(
-                () -> new RuntimeException("Host not found")
-        );
+                () -> new RuntimeException("Host not found"));
 
         Meeting meeting = new Meeting();
         String code = MeetingCodeGenerator.generateMeetingCode();
@@ -82,15 +82,13 @@ public class MeetingService {
 
         // Notify clients about new meeting
         messagingTemplate.convertAndSend(
-            "/topic/meetings",
-            Map.of(
-                "type", "MEETING_CREATED",
-                "meetingCode", meeting.getCode(),
-                "mediaRoomId", mediaRoomId,
-                "hostEmail", userEmail,
-                "hostName", host.getName()
-            )
-        );
+                "/topic/meetings",
+                Map.of(
+                        "type", "MEETING_CREATED",
+                        "meetingCode", meeting.getCode(),
+                        "mediaRoomId", mediaRoomId,
+                        "hostEmail", userEmail,
+                        "hostName", host.getName()));
 
         return new CreateMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(), "Meeting created");
     }
@@ -103,36 +101,34 @@ public class MeetingService {
             // For host: automatically join the MediaSoup room
             User host = meeting.getHost();
             MediaSoupService.User mediaUser = new MediaSoupService.User(
-                host.getId().toString(),
-                host.getName()
-            );
-            
+                    host.getId().toString(),
+                    host.getName());
+
+            meeting.setStartTime(Instant.now());
+            meeting.setStatus(MeetingStatus.ACTIVE);
+            meetingRepository.save(meeting);
+
             try {
                 mediaSoupService.joinRoom(meeting.getMediaRoomId(), mediaUser);
 
-                meetingRepository.updateStartTimeAndStatus(meeting.getId(), Instant.now(), MeetingStatus.ACTIVE);
-
                 // Notify others that host has joined
                 messagingTemplate.convertAndSend(
-                    "/topic/room/" + meeting.getCode(),
-                    Map.of(
-                        "type", "HOST_JOINED",
-                        "userId", host.getId().toString(),
-                        "name", host.getName()
-                    )
-                );
+                        "/topic/room/" + meeting.getCode(),
+                        Map.of(
+                                "type", "HOST_JOINED",
+                                "userId", host.getId().toString(),
+                                "name", host.getName()));
             } catch (Exception e) {
                 // Log error but allow host to join anyway
                 // This prevents MediaSoup issues from blocking meeting access
             }
-            
+
             return new JoinMeetingResponse(meeting.getCode(), meeting.getStatus(), ParticipantStatus.ACCEPTED, null);
         }
-        
+
         // If participant: create participant record and notify host
         User user = userRepository.findByEmail(userEmail).orElseThrow(
-            () -> new ResponseStatusException(HttpStatusCode.valueOf(404), "User not found")
-        );
+                () -> new ResponseStatusException(HttpStatusCode.valueOf(404), "User not found"));
 
         Participant participant = new Participant();
         participant.setUser(user);
@@ -142,18 +138,17 @@ public class MeetingService {
 
         // Notify host about join request
         messagingTemplate.convertAndSendToUser(
-            meeting.getHost().getEmail(),
-            "/queue/join-requests",
-            Map.of(
-                "type", "JOIN_REQUEST",
-                "meetingCode", code,
-                "participantId", participant.getId().toString(),
-                "userName", user.getName(),
-                "userEmail", userEmail
-            )
-        );
+                meeting.getHost().getEmail(),
+                "/queue/join-requests",
+                Map.of(
+                        "type", "JOIN_REQUEST",
+                        "meetingCode", code,
+                        "participantId", participant.getId().toString(),
+                        "userName", user.getName(),
+                        "userEmail", userEmail));
 
-        return new JoinMeetingResponse(meeting.getCode(), meeting.getStatus(), participant.getStatus(), participant.getId());
+        return new JoinMeetingResponse(meeting.getCode(), meeting.getStatus(), participant.getStatus(),
+                participant.getId());
     }
 
     @Transactional
@@ -172,10 +167,9 @@ public class MeetingService {
         // Add participant to MediaSoup room
         User user = participant.getUser();
         MediaSoupService.User mediaUser = new MediaSoupService.User(
-            user.getId().toString(),
-            user.getName()
-        );
-        
+                user.getId().toString(),
+                user.getName());
+
         try {
             mediaSoupService.joinRoom(meeting.getMediaRoomId(), mediaUser);
         } catch (Exception e) {
@@ -185,30 +179,26 @@ public class MeetingService {
 
         // Notify participant that they've been accepted
         messagingTemplate.convertAndSendToUser(
-            user.getEmail(),
-            "/queue/meeting-updates",
-            Map.of(
-                "type", "JOIN_ACCEPTED",
-                "meetingCode", code,
-                "mediaRoomId", meeting.getMediaRoomId()
-            )
-        );
-        
+                user.getEmail(),
+                "/queue/meeting-updates",
+                Map.of(
+                        "type", "JOIN_ACCEPTED",
+                        "meetingCode", code,
+                        "mediaRoomId", meeting.getMediaRoomId()));
+
         // Notify all participants about new member
         messagingTemplate.convertAndSend(
-            "/topic/room/" + meeting.getCode(),
-            Map.of(
-                "type", "PARTICIPANT_JOINED",
-                "participantId", participant.getId().toString(),
-                "userId", user.getId().toString(),
-                "name", user.getName(),
-                "initials", user.getName().substring(0, 1).toUpperCase(),
-                "isMuted", false,
-                "isCameraOff", false,
-                "isScreenSharing", false,
-                "isPinned", false
-            )
-        );
+                "/topic/room/" + meeting.getCode(),
+                Map.of(
+                        "type", "PARTICIPANT_JOINED",
+                        "participantId", participant.getId().toString(),
+                        "userId", user.getId().toString(),
+                        "name", user.getName(),
+                        "initials", user.getName().substring(0, 1).toUpperCase(),
+                        "isMuted", false,
+                        "isCameraOff", false,
+                        "isScreenSharing", false,
+                        "isPinned", false));
 
         return new AskToJoinMeetingResponse(code, participant.getStatus(), participant.getId());
     }
@@ -217,26 +207,24 @@ public class MeetingService {
     public AskToJoinMeetingResponse rejectMeeting(String code, String userEmail, String participantId) {
         Meeting meeting = getMeeting(code);
         if (!meeting.getHost().getEmail().equals(userEmail)) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(403),"You are not the host of the meeting");
+            throw new ResponseStatusException(HttpStatusCode.valueOf(403), "You are not the host of the meeting");
         }
-        
+
         Participant participant = participantRepository.findById(UUID.fromString(participantId))
-            .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404),"Participant not found"));
-        
+                .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Participant not found"));
+
         participant.setStatus(ParticipantStatus.REJECTED);
         participantRepository.save(participant);
-        
+
         // Notify participant they've been rejected
         User user = participant.getUser();
         messagingTemplate.convertAndSendToUser(
-            user.getEmail(),
-            "/queue/meeting-updates",
-            Map.of(
-                "type", "JOIN_REJECTED",
-                "meetingCode", code
-            )
-        );
-        
+                user.getEmail(),
+                "/queue/meeting-updates",
+                Map.of(
+                        "type", "JOIN_REJECTED",
+                        "meetingCode", code));
+
         return new AskToJoinMeetingResponse(code, participant.getStatus(), participant.getId());
     }
 
@@ -244,7 +232,7 @@ public class MeetingService {
     public CreateMeetingResponse endMeeting(String code, String userEmail) {
         Meeting meeting = getMeeting(code);
         if (!meeting.getHost().getEmail().equals(userEmail)) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(403),"You are not the host of the meeting");
+            throw new ResponseStatusException(HttpStatusCode.valueOf(403), "You are not the host of the meeting");
         }
 
         participantRepository.endMeeting(meeting.getId(), Instant.now());
@@ -261,12 +249,10 @@ public class MeetingService {
 
         // Notify all participants that meeting has ended
         messagingTemplate.convertAndSend(
-            "/topic/room/" + meeting.getCode(),
-            Map.of(
-                "type", "MEETING_ENDED",
-                "meetingCode", code
-            )
-        );
+                "/topic/room/" + meeting.getCode(),
+                Map.of(
+                        "type", "MEETING_ENDED",
+                        "meetingCode", code));
 
         return new CreateMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(), "Meeting ended");
     }
@@ -287,22 +273,21 @@ public class MeetingService {
 
         // Notify other participants
         messagingTemplate.convertAndSend(
-            "/topic/room/" + meeting.getCode(),
-            Map.of(
-                "type", "PARTICIPANT_LEFT",
-                "participantId", participant.getId().toString(),
-                "userId", userId.toString()
-            )
-        );
+                "/topic/room/" + meeting.getCode(),
+                Map.of(
+                        "type", "PARTICIPANT_LEFT",
+                        "participantId", participant.getId().toString(),
+                        "userId", userId.toString()));
 
-        return new CreateMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(), "Participant left the meeting");
+        return new CreateMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(),
+                "Participant left the meeting");
     }
 
     @Transactional
     public CreateMeetingResponse kickParticipant(String code, String userEmail, String participantId) {
         Meeting meeting = getMeeting(code);
         if (!meeting.getHost().getEmail().equals(userEmail)) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(403),"You are not the host of the meeting");
+            throw new ResponseStatusException(HttpStatusCode.valueOf(403), "You are not the host of the meeting");
         }
 
         Participant participant = getParticipant(UUID.fromString(participantId));
@@ -318,39 +303,37 @@ public class MeetingService {
 
         // Notify all participants including the kicked one
         messagingTemplate.convertAndSend(
-            "/topic/room/" + meeting.getCode(),
-            Map.of(
-                "type", "PARTICIPANT_KICKED",
-                "participantId", participant.getId().toString(),
-                "userId", participant.getUser().getId().toString()
-            )
-        );
-        
+                "/topic/room/" + meeting.getCode(),
+                Map.of(
+                        "type", "PARTICIPANT_KICKED",
+                        "participantId", participant.getId().toString(),
+                        "userId", participant.getUser().getId().toString()));
+
         // Send direct message to kicked participant
         messagingTemplate.convertAndSendToUser(
-            participant.getUser().getEmail(),
-            "/queue/meeting-updates",
-            Map.of(
-                "type", "YOU_WERE_KICKED",
-                "meetingCode", code
-            )
-        );
+                participant.getUser().getEmail(),
+                "/queue/meeting-updates",
+                Map.of(
+                        "type", "YOU_WERE_KICKED",
+                        "meetingCode", code));
 
-        return new CreateMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(), "Participant kicked from the meeting");
+        return new CreateMeetingResponse(meeting.getCode(), meeting.getTitle(), meeting.getStatus(),
+                "Participant kicked from the meeting");
     }
 
     private Meeting getMeeting(String code) {
         return meetingRepository.findByCode(code)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Meeting not found"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Meeting not found"));
     }
 
     private Participant getParticipant(UUID participantId) {
         return participantRepository.findById(participantId)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Participant not found"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404), "Participant not found"));
     }
 
     @Transactional
-    public ResponseEntity<Map<String, Object>> uploadRecording(@RequestBody Map<String, Object> request, String accessToken){
+    public ResponseEntity<Map<String, Object>> uploadRecording(@RequestBody Map<String, Object> request,
+            String accessToken) {
         try {
             // Validate access token
             if (accessToken == null) {
@@ -403,14 +386,12 @@ public class MeetingService {
             response.put("url", url);
             response.put("recordingType", recordingType);
 
-
             // Optionally notify other participants about the new recording
             try {
                 String notification = "New recording saved for meeting: " + meetingCode;
 
                 messagingTemplate.convertAndSend("/topic/meeting/" + meetingCode, Map.of(
-                        "type", "RECORDING_SAVED"
-                ));
+                        "type", "RECORDING_SAVED"));
                 System.out.println("Notified participants about new recording for meeting: " + meetingCode);
             } catch (Exception e) {
                 System.err.println("Error notifying participants about recording: " + e.getMessage());
