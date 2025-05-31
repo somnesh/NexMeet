29c92908d2c7f2bdba3c329541938519c5144011
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-05-31 22:35:22 +0530

    refactor: remove unused import statement from NexMeetApplication

--- server/src/main/java/com/nexmeet/NexMeetApplication.java
+++ server/src/main/java/com/nexmeet/NexMeetApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.Date;
-
 @SpringBootApplication
 @EnableTransactionManagement
 public class NexMeetApplication {
