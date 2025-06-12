e99b948ec34742ac5f8f7d4ffb00a11217de69bc
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-06-12 12:35:16 +0530

    feat: enhance CORS configuration with allowed headers for improved security

--- server/src/main/java/com/nexmeet/config/CorsConfig.java
+++ server/src/main/java/com/nexmeet/config/CorsConfig.java
@@ -13,9 +13,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://nex-meet-theta.vercel.app", "https://accounts.google.com/o/oauth2/auth")
+                        .allowedOrigins("http://localhost:5173", "https://nex-meet-theta.vercel.app",
+                                "https://accounts.google.com/o/oauth2/auth")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowCredentials(true);
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
