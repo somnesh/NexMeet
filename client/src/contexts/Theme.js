2f20391b8b0fb0cd4834ba555d2c03eab306989f
Somnesh Mukhopadhyay <86874926+somnesh@users.noreply.github.com>
2025-06-18 01:04:19 +0530

    fix: update default theme color from 'white' to 'light' in ThemeContext

--- client/src/contexts/Theme.js
+++ client/src/contexts/Theme.js
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
-  theme: "white",
+  theme: "light",
   darkTheme: () => {},
   lightTheme: () => {},
 });
